90232c40a78d8a7b24e99a6344cb8393e7257c39
benjamski <benjadamski@gmail.com>
2016-09-02 09:24:56 -0400

    Create js logic for submit action
    comments in add-todo.js describing use of input helper and onAdd property

--- app/components/add-todo.js
+++ app/components/add-todo.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  actions: {
+    submit() {
+      const text = this.get('text');
+      this.get('onAdd')(text);
+    }
+  }
+
 });
+
+//this.get() is Ember's way of getting a property
+
+// onAdd is one such a property: a closure action
+
+//Todo-widget component will call the add-todo component in components/todo-widget.hbs
